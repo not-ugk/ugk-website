f67ddcfbb13f04a02d0ee86fb292cf11d56ec94f
not-ugk <not.ugkaraoke@gmail.com>
2024-08-05 13:35:26 -0700

    make py script create data.json instead

--- csv-parser.py
+++ csv-parser.py
@@ -2,13 +2,16 @@ import csv
 import json
 import chevron
 
+#todo: add unit tests or switch the whole script to js (and then add tests)
+#todo: test for fixing chevron encodings 
+
 with open('dbexport.csv', newline='', encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile, fieldnames=['artist', 'title', 'id', 'filepath'])
     fieldsToKeep = ['artist', 'title']
     data = [dict((k, v) for k, v in row.items() if k in fieldsToKeep) for row in reader]
 
-with open('search-js.mustache', 'r', encoding="utf-8") as f:
-    with open('search.js', 'w', encoding = "utf-8") as jsfile:
+with open('data-json.mustache', 'r', encoding="utf-8") as f:
+    with open('data.json', 'w', encoding = "utf-8") as jsfile:
         newdata = map(lambda row: json.dumps(row), data)
         preparedjson = ','.join(newdata)
         result = chevron.render(f, { "songs": preparedjson })
