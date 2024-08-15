65dd9b9d45dc166755a2d06455c7661030fc5783
not-ugk <not.ugkaraoke@gmail.com>
2024-08-15 14:39:07 -0700

    change spaces in ids to underscroes

--- data/test_db_parser.py
+++ data/test_db_parser.py
@@ -1,4 +1,4 @@
-from db_parser import fix_encoding
+from db_parser import fix_encoding, fix_id
 
 def test_quote():
     assert fix_encoding(" &quot;blah&quot; ") == ' "blah" '
@@ -8,3 +8,7 @@ def test_ampersand():
 
 def test_acdc():
     assert fix_encoding(" AC?DC ") == ' AC/DC '
+
+def test_id_fix():
+    assert fix_id("bxSW 66ptUg") == 'bxSW_66ptUg'
+    assert fix_id(" bxSW66ptUg") == '_bxSW66ptUg'
